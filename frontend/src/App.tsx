4e3463ca5e8c82aa7d53e6b777047d34bcf93201
smritipradhan <smritipradhan545@gmail.com>
2023-12-10 21:18:29 +0530

    frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import styles from "./App.module.scss";
+
+function App() {
+  return <div className="App">Hello this is for testing</div>;
+}
+
+export default App;
---
+++ frontend/src/utils/apiUtils.ts
@@ -0,0 +1,3 @@
+export const axiosUtils = () => {
+  console.log("Utils");
+};
