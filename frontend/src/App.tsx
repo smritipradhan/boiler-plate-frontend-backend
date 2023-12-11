40545bd40f33bbd711c97ebd3d285f44f1a3f8eb
smritipradhan <smritipradhan545@gmail.com>
2023-12-11 18:42:10 +0530

    Frontend Backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@mui/material";
 import { useEffect } from "react";
 import styles from "./App.module.scss";
-import { getData, postData } from "./utils/apiUtils";
-import { Button } from "@mui/material";
+import { deleteData, getData, postData, putData } from "./utils/apiUtils";
+import { ErrorBoundary } from "react-error-boundary";
 
 const App = () => {
   useEffect(() => {
@@ -10,16 +11,60 @@ const App = () => {
     console.log({ d });
   }, []);
 
-  const postCallHandler = () => {
-    console.log("Post Call will be made");
-    postData("");
+  const postCallHandler = async () => {
+    try {
+      const data = await postData("", {
+        name: "Dummy Call for POST !!",
+        rating: 6.7,
+        price: 500,
+      });
+      console.log({ data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getCallHandler = async () => {
+    try {
+      const data = await getData("/6572f641bd65c8971a1a24d4");
+      console.log({ data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const putCallHandler = async () => {
+    try {
+      const data = await putData("/6572f641bd65c8971a1a24d4", {
+        rating: 10,
+        price: 2000,
+      });
+      console.log({ data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteCallHandler = async () => {
+    try {
+      const data = await deleteData("/6576f31c64b1819bb861b605");
+      console.log({ data });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
-    <div className={styles.app}>
-      Hello this is for testing
-      <Button onClick={postCallHandler}>Click for post</Button>
-    </div>
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <div className={styles.app}>
+        Hello this is for testing <br />
+        <Button onClick={postCallHandler}>POST</Button>
+        <br />
+        <Button onClick={getCallHandler}>GetBy ID</Button> <br />
+        <Button onClick={putCallHandler}>Put Call</Button> <br />
+        <Button onClick={deleteCallHandler}> Delete</Button>
+      </div>
+    </ErrorBoundary>
   );
 };
 
