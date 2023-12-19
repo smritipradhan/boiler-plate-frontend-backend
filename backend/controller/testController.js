027dc7678c28cea7e0aee6b27cd6c33d100f084e
smritipradhan <smritipradhan545@gmail.com>
2023-12-19 23:51:21 +0530

    Changes

--- backend/controller/testController.js
+++ backend/controller/testController.js
@@ -1,7 +1,18 @@
 // Name this file to testController.js
+import { Tour } from "./../models/TourModel.js";
+import APIFeatures from "./../utils/apiFeatures.js";
 
 export const getAll = async (req, res) => {
   try {
+    // For integration of features
+    // const features = new APIFeatures(Tour.find(), req.query)
+    //   .filter()
+    //   .sort()
+    //   .limitFields()
+    //   .paginate();
+
+    // const tours = await features.query;
+
     res.status(200).send({
       message: "success",
     });
